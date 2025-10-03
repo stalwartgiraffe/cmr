19b59c9d022095b79d6a1a5665a7d7e64284d6fb
Karl Meissner <karl.meissner@indexexchange.com>
2025-10-03 07:47:58 -0400

    fake wip

--- internal/gitlab/localhost/mergerequest.go
+++ internal/gitlab/localhost/mergerequest.go
@@ -1,98 +1,91 @@
 package localhost
 
 import (
+	"github.com/aarondl/opt/omitnull"
 	"time"
 )
 
-// MergeRequestV0 represents a GitLab merge request as per API_Entities_MergeRequestBasic
-type MergeRequestV0 struct {
-	ID                          int           `json:"id" fake:"{number:1,10000}"`
-	IID                         int           `json:"iid" fake:"{number:1,1000}"`
-	ProjectID                   int           `json:"project_id" fake:"{number:1,1000}"`
-	Title                       string        `json:"title" fake:"{sentence:8}"`
-	Description                 string        `json:"description,omitempty" fake:"{paragraph:3,5,10}"`
-	State                       string        `json:"state" fake:"{randomstring:[opened,closed,locked,merged]}"`
-	CreatedAt                   time.Time     `json:"created_at" fake:"{date}"`
-	UpdatedAt                   time.Time     `json:"updated_at" fake:"{date}"`
-	MergedBy                    *UserBasicV0  `json:"merged_by,omitempty"`
-	MergeUser                   *UserBasicV0  `json:"merge_user,omitempty"`
-	MergedAt                    time.Time     `json:"merged_at" fake:"{date}"`
-	ClosedBy                    *UserBasicV0  `json:"closed_by,omitempty"`
-	ClosedAt                    *string       `json:"closed_at,omitempty"`
-	TitleHTML                   string        `json:"title_html,omitempty"`       // TODO extend
-	DescriptionHTML             string        `json:"description_html,omitempty"` // TODO extend
-	TargetBranch                string        `json:"target_branch" fake:"{randomstring:[main,master,develop,staging]}"`
-	SourceBranch                string        `json:"source_branch" fake:"{word}"`
-	UserNotesCount              int           `json:"user_notes_count" fake:"{number:0,100}"`
-	Upvotes                     int           `json:"upvotes,omitempty" fake:"{number:0,50}"`
-	Downvotes                   int           `json:"downvotes,omitempty" fake:"{number:0,10}"`
-	Author                      *UserBasicV0  `json:"author,omitempty"`
-	Assignees                   []UserBasicV0 `json:"assignees,omitempty" fakesize:"0,3" `
-	Assignee                    *UserBasicV0  `json:"assignee,omitempty"`
-	Reviewers                   []UserBasicV0 `json:"reviewers,omitempty fakesize:"0,3" `
-	SourceProjectID             int           `json:"source_project_id,omitempty" fake:"{number:1,1000}"`
-	TargetProjectID             int           `json:"target_project_id,omitempty" fake:"{number:1,1000}"`
-	Labels                      []string      `json:"labels,omitempty" fakesize:"0,3" fake:"{word}"`
-	Draft                       bool          `json:"draft,omitempty" fake:"{bool}"`
-	Imported                    bool          `json:"imported,omitempty" fake:"{bool}"`
-	ImportedFrom                string        `json:"imported_from,omitempty" fake:"{randomstring:[none,github,bitbucket,gitlab]}"`
-	WorkInProgress              bool          `json:"work_in_progress,omitempty" fake:"{bool}"`
-	MergeWhenPipelineSucceeds   bool          `json:"merge_when_pipeline_succeeds,omitempty" fake:"{bool}"`
-	MergeStatus                 string        `json:"merge_status,omitempty" fake:"{randomstring:[can_be_merged,cannot_be_merged,unchecked]}"`
-	DetailedMergeStatus         string        `json:"detailed_merge_status,omitempty" fake:"{randomstring:[mergeable,broken_status,checking,ci_must_pass,ci_still_running,discussions_not_resolved,draft_status]}"`
-	MergeAfter                  string        `json:"merge_after,omitempty" fake:"{date}"`
-	SHA                         string        `json:"sha,omitempty" fake:"{uuid}"`
-	MergeCommitSHA              string        `json:"merge_commit_sha,omitempty" fake:"{uuid}"`
-	SquashCommitSHA             string        `json:"squash_commit_sha,omitempty" fake:"{uuid}"`
-	DiscussionLocked            bool          `json:"discussion_locked,omitempty" fake:"{bool}"`
-	ShouldRemoveSourceBranch    bool          `json:"should_remove_source_branch,omitempty" fake:"{bool}"`
-	ForceRemoveSourceBranch     bool          `json:"force_remove_source_branch,omitempty" fake:"{bool}"`
-	PreparedAt                  string        `json:"prepared_at,omitempty" fake:"{date}"`
-	AllowCollaboration          bool          `json:"allow_collaboration,omitempty" fake:"{bool}"`
-	AllowMaintainerToPush       bool          `json:"allow_maintainer_to_push,omitempty" fake:"{bool}"`
-	Reference                   string        `json:"reference,omitempty" fake:"{word}"`
-	WebURL                      string        `json:"web_url,omitempty" fake:"{url}"`
-	Squash                      bool          `json:"squash,omitempty" fake:"{bool}"`
-	SquashOnMerge               bool          `json:"squash_on_merge,omitempty" fake:"{bool}"`
-	TaskCompletionStatus        string        `json:"task_completion_status,omitempty" fake:"{word}"`
-	HasConflicts                bool          `json:"has_conflicts,omitempty" fake:"{bool}"`
-	BlockingDiscussionsResolved bool          `json:"blocking_discussions_resolved,omitempty" fake:"{bool}"`
-	ApprovalsBeforeMerge        int           `json:"approvals_before_merge,omitempty" fake:"{number:0,10}"`
+// MergeRequest represents a GitLab merge request
+type MergeRequest struct {
+	ID                          int                  `json:"id"`
+	IID                         int                  `json:"iid"`
+	ProjectID                   int                  `json:"project_id"`
+	Title                       string               `json:"title"`
+	Description                 string               `json:"description"`
+	State                       string               `json:"state"`
+	CreatedAt                   time.Time            `json:"created_at"`
+	UpdatedAt                   time.Time            `json:"updated_at"`
+	MergedBy                    *UserBasic           `json:"merged_by"`
+	MergeUser                   *UserBasic           `json:"merge_user"`
+	MergedAt                    *time.Time           `json:"merged_at"`
+	ClosedBy                    *UserBasic           `json:"closed_by"`
+	ClosedAt                    *time.Time           `json:"closed_at"`
+	TargetBranch                string               `json:"target_branch"`
+	SourceBranch                string               `json:"source_branch"`
+	UserNotesCount              int                  `json:"user_notes_count"`
+	Upvotes                     int                  `json:"upvotes"`
+	Downvotes                   int                  `json:"downvotes"`
+	Author                      UserBasic            `json:"author"`
+	Assignees                   []UserBasic          `json:"assignees"`
+	Assignee                    *UserBasic           `json:"assignee"`
+	Reviewers                   []UserBasic          `json:"reviewers"`
+	SourceProjectID             int                  `json:"source_project_id"`
+	TargetProjectID             int                  `json:"target_project_id"`
+	Labels                      []string             `json:"labels"`
+	Draft                       bool                 `json:"draft"`
+	WorkInProgress              bool                 `json:"work_in_progress"`
+	Milestone                   omitnull.Val[string] `json:"milestone"`
+	MergeWhenPipelineSucceeds   bool                 `json:"merge_when_pipeline_succeeds"`
+	MergeStatus                 string               `json:"merge_status"`
+	DetailedMergeStatus         string               `json:"detailed_merge_status"`
+	SHA                         string               `json:"sha"`
+	MergeCommitSHA              omitnull.Val[string] `json:"merge_commit_sha"`
+	SquashCommitSHA             omitnull.Val[string] `json:"squash_commit_sha"`
+	DiscussionLocked            omitnull.Val[bool]   `json:"discussion_locked"`
+	ShouldRemoveSourceBranch    omitnull.Val[bool]   `json:"should_remove_source_branch"`
+	ForceRemoveSourceBranch     bool                 `json:"force_remove_source_branch"`
+	Reference                   string               `json:"reference"`
+	References                  References           `json:"references"`
+	WebURL                      string               `json:"web_url"`
+	TimeStats                   TimeStats            `json:"time_stats"`
+	Squash                      bool                 `json:"squash"`
+	SquashOnMerge               bool                 `json:"squash_on_merge"`
+	TaskCompletionStatus        TaskCompletionStatus `json:"task_completion_status"`
+	HasConflicts                bool                 `json:"has_conflicts"`
+	BlockingDiscussionsResolved bool                 `json:"blocking_discussions_resolved"`
+	ApprovalsBeforeMerge        omitnull.Val[int]    `json:"approvals_before_merge"`
 }
 
-// TODO get declarative query parser package?
-// MergeRequestsQueryParamsV0 represents the query parameters for the merge requests endpoint
-type MergeRequestsQueryParamsV0 struct {
-	PageQueryParams
+// MergeRequestList represents a list of merge requests
+type MergeRequestList []MergeRequest
 
-	AuthorID               *int       `json:"author_id,omitempty" fake:"{number:100,999}"`
-	AuthorUsername         string     `json:"author_username,omitempty" fake:"{username}"`
-	AssigneeID             *int       `json:"assignee_id,omitempty" fake:"{number:100,999}"`
-	AssigneeUsername       []string   `json:"assignee_username,omitempty" fake:"{tags}"`
-	ReviewerUsername       string     `json:"reviewer_username,omitempty" fake:"{username}"`
-	ReviewerID             *int       `json:"reviewer_id,omitempty" fake:"{number:100,999}"`
-	Labels                 []string   `json:"labels,omitempty" fake:"{tags}"`
-	Milestone              string     `json:"milestone,omitempty" fake:"{word}"`
-	MyReactionEmoji        string     `json:"my_reaction_emoji,omitempty" fake:"{emoji}"`
-	State                  string     `json:"state,omitempty" fake:"{randomstring:[opened,closed,locked,merged,all]}"`
-	OrderBy                string     `json:"order_by,omitempty" fake:"{randomstring:[created_at,updated_at,title]}"`
-	Sort                   string     `json:"sort,omitempty" fake:"{randomstring:[asc,desc]}"`
-	WithLabelsDetails      bool       `json:"with_labels_details,omitempty" fake:"{bool}"`
-	WithMergeStatusRecheck bool       `json:"with_merge_status_recheck,omitempty" fake:"{bool}"`
-	CreatedAfter           *time.Time `json:"created_after,omitempty" fake:"{date}"`
-	CreatedBefore          *time.Time `json:"created_before,omitempty" fake:"{date}"`
-	UpdatedAfter           *time.Time `json:"updated_after,omitempty" fake:"{date}"`
-	UpdatedBefore          *time.Time `json:"updated_before,omitempty" fake:"{date}"`
-	View                   string     `json:"view,omitempty" fake:"{randomstring:[simple,normal]}"`
-	Scope                  string     `json:"scope,omitempty" fake:"{randomstring:[created-by-me,assigned-to-me,all]}"`
-	SourceBranch           string     `json:"source_branch,omitempty" fake:"{word}"`
-	SourceProjectID        *int       `json:"source_project_id,omitempty" fake:"{number:100,999}"`
-	TargetBranch           string     `json:"target_branch,omitempty" fake:"{randomstring:[main,master,develop]}"`
-	Search                 string     `json:"search,omitempty" fake:"{word}"`
-	In                     string     `json:"in,omitempty" fake:"{randomstring:[title,description]}"`
-	WIP                    string     `json:"wip,omitempty" fake:"{randomstring:[yes,no]}"`
-	NotAuthorID            *int       `json:"not_author_id,omitempty"`
-	NotAuthorUsername      string     `json:"not_author_username,omitempty" fake:"{username}"`
-	NotAssigneeID          *int       `json:"not_assignee_id,omitempty" fake:"{number:100,999}"`
-	NotAssigneeUsername    []string   `json:"not_assignee_username,omitempty" fake:"{tags}"`
+// UserBasic represents a GitLab user
+type UserBasic struct {
+	ID        int                  `json:"id"`
+	Username  string               `json:"username"`
+	Name      string               `json:"name"`
+	State     string               `json:"state"`
+	AvatarURL omitnull.Val[string] `json:"avatar_url"`
+	WebURL    string               `json:"web_url"`
+}
+
+// References contains reference strings for the merge request
+type References struct {
+	Short    string `json:"short"`
+	Relative string `json:"relative"`
+	Full     string `json:"full"`
+}
+
+// TimeStats contains time tracking statistics
+type TimeStats struct {
+	TimeEstimate        int                  `json:"time_estimate"`
+	TotalTimeSpent      int                  `json:"total_time_spent"`
+	HumanTimeEstimate   omitnull.Val[string] `json:"human_time_estimate"`
+	HumanTotalTimeSpent omitnull.Val[string] `json:"human_total_time_spent"`
+}
+
+// TaskCompletionStatus tracks task completion in the merge request
+type TaskCompletionStatus struct {
+	Count          int `json:"count"`
+	CompletedCount int `json:"completed_count"`
 }
